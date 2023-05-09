80e5401ccbab7fe942913cfbfae8e71255347be4
Michael Chau <chaumdevs@gmail.com>
2023-05-08 21:54:13 -0400

    Tkinter data entry

---
+++ data_entry_app.py
@@ -0,0 +1,290 @@
+#the ABQ Data Entry application
+import tkinter as tk
+from tkinter import ttk
+from datetime import datetime #to generate Date strings
+from pathlib import Path #save routine
+import csv # to interact with csv
+
+variables = dict()
+records_saved = 0
+
+root = tk.Tk()
+root.title("ABQ Data Entry Application")
+root.columnconfigure(0, weight=1)
+
+ttk.Label(
+    root,
+    text="ABQ Data Entry Application",
+    font=("TkDefaultFont",16)
+).grid()
+
+drf = ttk.Frame(root)
+drf.grid(padx=10, sticky="ew")
+drf.columnconfigure(0, weight=1)
+
+r_info = ttk.LabelFrame(
+    drf,
+    text="Record Information")
+r_info.grid(sticky="we")
+for i in range(3):
+    r_info.columnconfigure(i, weight=1)
+
+variables["Date"] = tk.StringVar()
+ttk.Label(
+    r_info,
+    text="Date"
+).grid(row=0, column=0)
+ttk.Entry(
+    r_info,
+    textvariable=variables["Date"]
+).grid(row=1, column=0, sticky="we")
+
+time_values = ["8:00", "12:00",  "16:00", "20:00"]
+variables["Time"] = tk.StringVar()
+ttk.Label(
+    r_info,
+    text="Time"
+).grid(row=0, column=1)
+ttk.Combobox(
+    r_info,
+    textvariable=variables["Time"],
+    values=time_values
+).grid(row=1, column=1, sticky="we")
+
+variables["Technician"] = tk.StringVar()
+tk.Label(
+    r_info,
+    text="Technician"
+).grid(row=0, column=2)
+ttk.Entry(
+    r_info,
+    textvariable=variables["Technician"]
+).grid(row=1, column=2, sticky="we")
+
+variables["Lab"] = tk.StringVar()
+ttk.Label(
+    r_info,
+    text="Lab"
+).grid(row=2, column=0)
+labframe = ttk.Frame(r_info)
+for lab in ("A","B","C"):
+    ttk.Radiobutton(
+        labframe, value=lab, text=lab, variable=variables["Lab"]
+    ).pack(side="left", expand=True)
+labframe.grid(row=3, column=0, sticky="we")
+
+variables["Plot"] = tk.IntVar()
+ttk.Label(
+    r_info,
+    text="Plot"
+).grid(row=2,column=1)
+ttk.Combobox(
+    r_info,
+    textvariable=variables["Plot"],
+    values=list(range(1,21))
+).grid(row=3, column=1, sticky="we")
+
+variables["Seed Sample"] = tk.StringVar()
+ttk.Label(
+    r_info,
+    text="Seed Sample"
+).grid(row=2, column=2)
+ttk.Entry(
+    r_info,
+    textvariable=variables["Seed Sample"]
+).grid(row=3, column=2, sticky="we")
+
+e_info = ttk.LabelFrame(
+    drf,
+    text="Environment Data"
+)
+e_info.grid(sticky="we")
+for i in range(3):
+    e_info.columnconfigure(i, weight=1)
+
+variables["Humidity"] = tk.StringVar()
+ttk.Label(
+    e_info,
+    text="Humidity (g/m)"
+).grid(row=0, column=0)
+ttk.Spinbox(
+    e_info,
+    textvariable=variables["Humidity"],
+    from_=0.5, to=52.0, increment=0.01
+).grid(row=1, column=0, sticky="we")
+
+variables["Light"] = tk.DoubleVar()
+ttk.Label(
+    e_info,
+    text="Light (klx)"
+).grid(row=0, column=1)
+ttk.Spinbox(
+    e_info,
+    textvariable=variables["Light"],
+    from_=0, to=100, increment=0.01
+).grid(row=1, column=1, sticky="we")
+
+variables["Temperature"] = tk.DoubleVar()
+ttk.Label(
+    e_info,
+    text="Temperature (C)"
+).grid(row=0, column=2)
+ttk.Spinbox(
+    e_info,
+    textvariable=variables["Temperature"],
+    from_=4, to=40, increment=0.01
+).grid(row=1, column=2, sticky="we")
+
+variables["Equipment Fault"] = tk.BooleanVar(value=False)
+ttk.Checkbutton(
+    e_info,
+    variable=variables["Equipment Fault"],
+    text="Equipment Fault"
+).grid(row=2, column=0, sticky="w", pady=5)
+
+#The Plant Data Section
+
+p_info = tk.LabelFrame(
+    drf,
+    text="Plant Data"
+)
+p_info.grid(sticky="we")
+for i in range(3):
+    p_info.columnconfigure(i, weight=1)
+
+variables["Plants"] = tk.IntVar()
+ttk.Label(
+    p_info,
+    text="Plants"
+).grid(row=0,column=0)
+ttk.Spinbox(
+    p_info,
+    textvariable=variables["Plants"],
+    from_=0, to=20, increment=1
+).grid(row=1, column=0, sticky="we")
+
+variables["Blossoms"] = tk.IntVar()
+ttk.Label(
+    p_info,
+    text="Blossoms"
+).grid(row=0, column=1)
+ttk.Spinbox(
+    p_info,
+    textvariable=variables["Blossoms"],
+    from_=0, to=1000, increment=1
+).grid(row=1, column=1, sticky="we")
+
+variables["Fruit"] = tk.IntVar()
+ttk.Label(
+    p_info,
+    text="Fruit"
+).grid(row=0, column=2)
+ttk.Spinbox(
+    p_info,
+    textvariable=variables["Fruit"],
+    from_=0, to=1000, increment=1
+).grid(row=1, column=2, sticky=("we"))
+
+variables["Min Height"] = tk.DoubleVar()
+ttk.Label(
+    p_info,
+    text="Min Height (cm)"
+).grid(row=2, column=0)
+ttk.Spinbox(
+    p_info,
+    textvariable=variables["Min Height"],
+    from_=0, to=1000, increment=0.01
+).grid(row=3, column=0, sticky="we")
+
+variables["Max Height"] = tk.DoubleVar()
+ttk.Label(
+    p_info,
+    text="Max Height (cm)"
+).grid(row=2,column=1)
+ttk.Spinbox(
+    p_info,
+    textvariable=variables["Max Height"],
+    from_=0, to=1000, increment=0.01
+).grid(row=3, column=1, sticky="we")
+
+variables["Med Height"] = tk.DoubleVar()
+ttk.Label(
+    p_info,
+    text="Median Height (cm)"
+).grid(row=2,column=2)
+ttk.Spinbox(
+    p_info,
+    textvariable=variables["Med Height"],
+    from_=0, to=1000, increment=0.01
+).grid(row=3, column=2, sticky="we")
+
+ttk.Label(
+    drf,
+    text="Notes"
+).grid()
+notes_inp = tk.Text(
+    drf,
+    width=75,
+    height=10
+)
+notes_inp.grid(sticky="we")
+
+buttons = tk.Frame(drf)
+buttons.grid(sticky="we")
+
+def on_reset():
+    for variable in variables.values():
+        if isinstance(variable, tk.BooleanVar):
+            variable.set(False)
+        else:
+            variable.set("")
+    notes_inp.delete("1.0", tk.END)
+
+
+status_variable = tk.StringVar()
+ttk.Label(
+    root,
+    textvariable=status_variable
+).grid(sticky="we", row=99, padx=10)
+
+
+def on_save():
+    global records_saved
+    datestring = datetime.today().strftime("%Y-%m-%d")
+    filename = f"abq_data_record_{datestring}.csv"
+    newfile = not Path(filename).exists()
+    data = dict()
+    fault = variables["Equipment Fault"].get()
+    for key, variable in variables.items():
+        if fault and key in ("Light","Humidity","Temperature"):
+            data[key]=""
+        else:
+            try:
+                data[key]=variable.get()
+            except tk.TclError:
+                status_variable.set(
+                    f"Error in field:{key}. Data was not saved!"
+                )
+                return
+    data["Notes"] = notes_inp.get("1.0",tk.END)
+    with open(filename,"a",newline="")as fh:
+        csvwriter = csv.DictWriter(fh, fieldnames=data.keys())
+        if newfile:
+            csvwriter.writeheader()
+            csvwriter.writerow(data)
+        records_saved+=1
+        status_variable.set(
+            f"{records_saved} records saved this session"
+        )
+        on_reset()
+
+
+
+save_button = ttk.Button(buttons,text="Save", command=on_save)
+save_button.pack(side="right")
+
+reset_button = ttk.Button(buttons, text="Reset", command=on_reset)
+reset_button.pack(side="right")
+
+
+root.mainloop()
